50ace41d9e20d2a317ed58002c03cea98f43ea07
CodePorting <mohsan.raza@codeporting.com>
2012-05-23 14:51:26 +0500

    Addition of new file to test code

---
+++ 1.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace DvdCopyWord.WindowsAPI
+{
+    class _1
+    {
+		private string csTestStringVariable;
+		public string csPath;
+    }
+}
