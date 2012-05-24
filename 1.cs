70b2c86761ac66cca886edf7f4cc3c5e50c2edbe
CodePorting <mohsan.raza@codeporting.com>
2012-05-24 11:41:44 +0500

    Addition of new file to test code

--- 1.cs
+++ 1.cs
@@ -8,5 +8,21 @@ namespace DvdCopyWord.WindowsAPI
     {
 		private string csTestStringVariable;
 		public string csPath;
+		
+		publuc TestFunction()
+		{
+			DirectoryInfo di = new System.IO.DirectoryInfo("Sample path here");;
+			DirectoryInfo parent = Directory.GetParent(di.FullName);
+			String newPath = Path.Combine(parent.FullName, "sample input path");
+
+			// rename to some temp name, to help change lower and uppercast names
+			di.MoveTo(newPath + "__renameTemp__");
+			di.MoveTo(newPath);
+
+			// Trying to cleanup to prevent directory locking, doesn't work...
+			di = null;
+			parent = null;
+			GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
+		}
     }
 }
