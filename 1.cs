f0a281f0f86f84ad0763bff3872c8470b3b84b56
CodePorting <mohsan.raza@codeporting.com>
2012-06-28 23:37:19 +0600

    Update master

--- 1.cs
+++ 1.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 namespace DvdCopyWord.WindowsAPI
 {
-    public class TestInheritence: ParentTestClass 
+    public class TestInheritence: ParentTestClass //Sheraz Test1: Inheritance conversion in JAVA
     {
 		private string csTestStringVariable;
 		public string csPath;
@@ -34,9 +34,10 @@ namespace DvdCopyWord.WindowsAPI
 
 		private Static Integer TestFunc2()
 		{
-			string s= cspath??string.Empty;
-			///	 test data here....
-			return new Integer();
+			string s= cspath??string.Empty;  //Sheraz Test2: Null operation transaltion in Java
+			
+			Integer? count= new Integer (); 
+			return count;
 		}
 
 		protected String TestFunc3()
