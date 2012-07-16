0cdcc3adc97b5dbe36b29d9c249a926e49e121c2
CodePorting <mohsan.raza@codeporting.com>
2012-07-16 14:21:52 +0500

    CSharp Code Update

--- 1.cs
+++ 1.cs
@@ -9,7 +9,7 @@ namespace DvdCopyWord.WindowsAPI
 		private string csTestStringVariable;
 		public string csPath;
                 public string csPath1;
-                public string csPath2;
+                public string csPath3;
 
 		public TestFunction()
 		{
